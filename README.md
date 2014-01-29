de420ebbf563e62111a225c43d213a49db89a0fc
Jason Galea <jason@lecstor.com>
2014-01-29 22:18:08 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::PDFUnicorn - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::PDFUnicorn;
+
+# DESCRIPTION
+
+Net::PDFUnicorn is
+
+# AUTHOR
+
+Jason Galea <lecstor@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Jason Galea
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/PDFUnicorn.pm
@@ -0,0 +1,39 @@
+package Net::PDFUnicorn;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::PDFUnicorn - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::PDFUnicorn;
+
+=head1 DESCRIPTION
+
+Net::PDFUnicorn is
+
+=head1 AUTHOR
+
+Jason Galea E<lt>lecstor@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Jason Galea
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
