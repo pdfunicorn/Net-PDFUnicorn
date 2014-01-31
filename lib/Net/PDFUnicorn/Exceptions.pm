5a766f6bc9c2b82afd4aaf2247420b90473dc25f
Jason Galea <jason@lecstor.com>
2014-01-31 23:41:46 +1000

    added Not Found

--- lib/Net/PDFUnicorn/Exceptions.pm
+++ lib/Net/PDFUnicorn/Exceptions.pm
@@ -7,40 +7,37 @@ our $VERSION = '0.01';
 use Exception::Class (
     'PDFU::Exception' => {
         fields => [ 'code', 'errors' ],
+#        alias  => 'throw_fields',
     },
 
     'PDFU::InvalidRequestError' => {
         isa => 'PDFU::Exception',
         description => 'The request was invalid',
-#        fields => [ 'grandiosity', 'quixotic' ],
-#        alias  => 'throw_fields',
     },
 
     'PDFU::AuthenticationError' => {
         isa => 'PDFU::Exception',
         description => 'The api key was invalid',
-#        fields => [ 'grandiosity', 'quixotic' ],
-#        alias  => 'throw_fields',
     },
 
     'PDFU::APIConnectionError' => {
         isa => 'PDFU::Exception',
         description => 'There was an error connecting to the PDFUnicorn service',
-#        fields => [ 'grandiosity', 'quixotic' ],
-#        alias  => 'throw_fields',
     },
 
     'PDFU::TemporaryError' => {
         isa => 'PDFU::Exception',
         description => 'This resource is temporarily unavailable',
         fields => [ 'retry_after' ],
-#        alias  => 'throw_fields',
     },
 
     'PDFU::PDFUnicornError' => {
         isa => 'PDFU::Exception',
         description => 'PDFUnicorn had a meltdown',
-#        fields => [ 'grandiosity', 'quixotic' ],
-#        alias  => 'throw_fields',
+    },
+    
+    'PDFU::NotFound' => {
+        isa => 'PDFU::Exception',
+        description => 'Resource Not Found',
     },
 );
